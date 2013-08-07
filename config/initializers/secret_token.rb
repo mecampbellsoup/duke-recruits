9544ec3dc74484190dfc1c26730fdd8fe36df8b3
mecampbellsoup <mecampbell25@gmail.com>
2013-08-07 17:09:27 -0400

    restarting this app for practice

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DukeRecruits::Application.config.secret_key_base = '7f8f1ee79019089d121250e83c8954f876c863124f06f30a370bc66bccf336f1b32951a5a20c2533e241289ef3ea917db515fa1ebd7efb0f0c651c68b31d343e'
