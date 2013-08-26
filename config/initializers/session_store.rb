82f8bd76538c3b451f3ef2475f347400b7794b09
mecampbellsoup <mecampbell25@gmail.com>
2013-08-26 19:03:08 -0400

    adding session_store initializer back

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,4 @@
+# Be sure to restart your server when you modify this file.
+
+DukeRecruits::Application.config.session_store :cookie_store, key: '_duke_recruits_session'
+#DukeRecruits::Application.config.session_store :active_record_store
